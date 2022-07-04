9f8b4e5a8a8500eda081dd5b2910ead1a310f24c
Evina Mouselimi <evina@cgworks.com>
2022-07-04 14:34:38 +0300

    assignment 5 complete

--- assignement5-solution/assignment5-solution-starter/js/script.js
+++ assignement5-solution/assignment5-solution-starter/js/script.js
@@ -99,9 +99,9 @@ $(function () {
 				// variable's name implies it expects.
 
 				var chosenCategoryShortName =
-					chooseRandomCategory(categories).short_name;
+					"'" + chooseRandomCategory(categories).short_name + "'";
 
-				// console.log(chosenCategoryShortName); // random string
+				console.log(chosenCategoryShortName); // random string
 
 				// TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
 				// chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -114,11 +114,13 @@ $(function () {
 				// Hint: you need to surround the chosen category short name with something before inserting
 				// it into the home html snippet.
 				//
-				var homeHtmlToInsertIntoMainPage = insertProperty(
-					homeHtml,
-					randomCategoryShortName,
+				var homeHtmlToInsertIntoMainPage = homeHtml;
+				homeHtmlToInsertIntoMainPage = insertProperty(
+					homeHtmlToInsertIntoMainPage,
+					"randomCategoryShortName",
 					chosenCategoryShortName
 				);
+				insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
 
 				// TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
 				// Use the existing insertHtml function for that purpose. Look through this code for an example
